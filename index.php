35d62c1a6ae4441e147f4669ab9a8f9b6ad880e1
Gabriel-Borges12 <borgesgs369@gmail.com>
2023-09-25 13:51:28 -0300

    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Pet TRICOLOR</title>
 </head>
 
@@ -37,16 +37,53 @@
             <div class="asd-title">
                 <h2>Opções</h2>
             </div>
-                <div class="asd-wrap">
-                    <ul class="asd-list">
-                        <li class="nav-link"><a href="./agendamento/consulta_agenda.php">Agendamentos</a></li>
-                        <li class="nav-link"><a href="./agendamento/consulta_animal.php">Animais</a></li>
-                        <li class="nav-link"><a href="./agendamento/consulta_cliente.php">Clientes</a></li>
-                    </ul>
-                </div>
+            <div class="asd-wrap">
+                <ul class="asd-list">
+                    <li class="nav-link"><a href="./agendamento/consulta_agenda.php">Agendamentos</a></li>
+                    <li class="nav-link"><a href="./agendamento/consulta_animal.php">Animais</a></li>
+                    <li class="nav-link"><a href="./agendamento/consulta_cliente.php">Clientes</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     <footer>
+        <div class="conteudogeral">
+            <div class="conteudo1">
+                <h4>Sobre nós</h4><br>
+                <p>Ajudamos e cuidamos dos seus pets,</p>
+                <p>através do amor e carinho</p>
+                <p>envolvido em nós!</p>
+            </div>
+            <div class="conteudo2">
+                <h4>Links Importantes</h4><br>
+                <ul class="linksfooter">
+                    <li><a class="linksimportantes" href="#">Funcionalidades</a></li>
+                    <li><a class="linksimportantes" href="#">Termos de uso</a></li>
+                    <li><a class="linksimportantes" href="#">Dúvidas</a></li>
+                    <li><a class="linksimportantes" href="#">Planos</a></li>
+                    <li><a class="linksimportantes" href="#">Blog</a></li>
+                </ul>
+
+            </div>
+            <div class="conteudo3">
+                <h4>Siga nossas redes sociais!</h4>
+                <div class="social-icons">
+                    <!-- ícones encontrados no site icons8.com.br -->
+                    <a href="https://www.facebook.com/?locale=pt_BR" class="social-icon"><img
+                            src="img/icons8-facebook-48.png" alt="Facebook"></a>
+                    <a href="https://twitter.com/login?lang=pt" class="social-icon"><img
+                            src="img/icons8-twitter-48 (1).png" alt="Twitter"></a>
+                    <a href="https://www.instagram.com/" class="social-icon"><img src="img/icons8-instagram-50 (1).png"
+                            alt="Instagram"></a>
+                    <br><br>
+                    <h5>Dúvidas ou sugestões?</h5>
+                    <a href="#" class="linkemail">contact@pettricolor</a><br>
+                </div>
+            </div>
+        </div>
+        </div>
+    </footer>
+    <!-- <footer>
         <img src="./img/logo_petshop.png" alt="Logo Petshop" id="foot-img">
         <div class="foot-wrap">
             <fieldset>
@@ -57,7 +94,7 @@
                 </ul>
             </fieldset>
         </div>
-    </footer>
+    </footer> -->
 </body>
 
 </html>
